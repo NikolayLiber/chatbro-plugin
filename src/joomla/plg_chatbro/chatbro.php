a8157ee972cc4d00feac4dc3eceaac18c897d19d
Nikolay Liber <fwnd80@gmail.com>
2017-02-15 16:28:19 +0500

    Begin to code plugin for popup chat

---
+++ src/joomla/plg_chatbro/chatbro.php
@@ -0,0 +1,23 @@
+<?php
+
+defined('_JEXEC') or die;
+
+class PlgSystemChatBro extends JPlugin {
+    function onAfterRender() {
+      ob_start();
+      ?>
+        <script id="chatbro">
+          console.log('Hello from chatbro!!!');
+        </script>
+      <?php
+
+      $script = ob_get_contents();
+      ob_end_clean();
+
+      $app = JFactory::getApplication();
+
+      $app->appendBody($script);
+    }
+}
+
+?>
