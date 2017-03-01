0270bf72cfa95396854678a0272643637fb7e1f1
Nikolay Liber <fwnd80@gmail.com>
2017-03-01 22:09:56 +0500

    build dev browser code is almost ready

--- Gruntfile.js
+++ Gruntfile.js
@@ -34,7 +34,7 @@ module.exports = function (grunt) {
     sass: {
       common: {
         files: {
-          '_build/common/css/chatbro.css': 'src/browser/scss/chatbro.scss'
+          '_build/common/css/chatbro.css': 'src/browser/common/scss/chatbro.scss'
         }
       }
     },
@@ -42,17 +42,44 @@ module.exports = function (grunt) {
     concat: {
       joomla_devcss: {
         src: ['_build/common/css/common.css', '_build/common/css/chatbro.css'],
-        dest: ['_build/joomla/css/chatbro.css']
+        dest: '_build/joomla/css/chatbro.css'
       },
 
       joomla_devjs: {
         src: ['_build/common/js/common.js', 'src/browser/common/js/*.js'],
-        dest: ['_build/joomla/js/chatbro.js']
+        dest: '_build/joomla/js/chatbro.js'
       }
     },
 
     eslint: {
-      target: 'src/browser/js/**.js'
+      target: 'src/browser/**/*.js'
+    },
+
+    copy: {
+      fonts: {
+        expand: true,
+        src: ['bower_components/font-awesome/fonts/*', 'bower_components/bootstrap/fonts/*'],
+        dest: '_build/common/fonts/',
+        filter: 'isFile',
+        flatten: true
+      }
+    },
+
+    symlink: {
+      options: {
+        overwrite: true
+      },
+
+      joomla: {
+        files: [
+          { src: '_build/common/fonts',
+            dest: '_build/joomla/fonts' },
+          { src: 'src/server/common',
+            dest: 'src/server/platforms/joomla/lib_chatbro/common' },
+          { src: '_build/joomla',
+            dest: 'src/server/platforms/joomla/com_chatbro/media' }
+        ]
+      }
     }
   })
 
@@ -60,9 +87,11 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-bower-concat')
   grunt.loadNpmTasks('grunt-sass')
   grunt.loadNpmTasks('grunt-contrib-concat')
+  grunt.loadNpmTasks('grunt-contrib-copy')
   grunt.loadNpmTasks('grunt-eslint')
+  grunt.loadNpmTasks('grunt-contrib-symlink')
 
   grunt.registerTask('build:joomla:css:dev', ['bower_concat:common_css', 'sass:common', 'concat:joomla_devcss'])
   grunt.registerTask('build:joomla:js:dev', ['bower_concat:common_js', 'eslint', 'concat:joomla_devjs'])
-  grunt.registerTask('build:joomla:dev', ['build:joomla:css:dev', 'build:joomla:js:dev'])
+  grunt.registerTask('build:joomla:dev', ['build:joomla:css:dev', 'build:joomla:js:dev', 'copy:fonts', 'symlink:joomla'])
 }
